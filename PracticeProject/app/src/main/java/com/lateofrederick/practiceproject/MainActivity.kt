09487c81d3a8dcf5085710ce6fcb895217887a49
lateo16 <lateofrederick@gmail.com>
2020-09-03 12:41:17 +0000

    implemented recycler adapter for both learning leaders and skill iq

--- PracticeProject/app/src/main/java/com/lateofrederick/practiceproject/MainActivity.kt
+++ PracticeProject/app/src/main/java/com/lateofrederick/practiceproject/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.lateofrederick.practiceproject
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.fragment.app.FragmentActivity
 import com.lateofrederick.practiceproject.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+//        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
@@ -20,11 +18,5 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
